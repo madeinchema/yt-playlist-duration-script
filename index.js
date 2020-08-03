b494ed02f88ccdb654bb7e406873e97a61f1b591
madeinchema <chemavg1@gmail.com>
2020-08-03 23:01:35 +0200

    Get total time, format it and log it.

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
-// Durations' List Test
+/**
+ * Get curated list of video durations
+ */
+
 const durations = [
+  '3:15',
+  '3:56',
+  '6:31',
+  '7:19',
+  '4:57',
+  '12:32',
+  '7:42',
+  '4:47',
+  '6:03',
+  '12:19',
+  '3:31',
+  '4:29',
+  '6:41',
+  '7:10',
+  '7:31',
+  '4:13',
+  '4:41',
   '6:46',
-  '10:31',
-  '7:35',
-  '7:36',
-  '8:36',
-  '5:08',
-  '6:50',
-  '8:16',
-  '3:04',
-  '7:46',
-  '11:19',
-  '8:47',
-  '6:06',
-  '6:37',
-  '↵      7:19↵    ',
-  '↵      11:09↵    ',
-  '↵      10:10↵    ',
-  '↵      9:44↵    ',
-  '↵      3:25↵    ',
-  '↵      6:51↵    ',
-  '↵      2:03↵    ',
+  '8:00',
+  '↵      5:34↵    ',
+  '↵      4:40↵    ',
+  '↵      5:07↵    ',
+  '↵      10:32↵    ',
+  '↵      4:06↵    ',
+  '↵      7:31↵    ',
+  '↵      3:49↵    ',
 ].map((time) => {
   const regex = /\d+:\d{2}/g;
   return time.match(regex).toString();
 });
 
-console.log(durations);
+/**
+ * Get total time
+ */
+
+const timeSeconds = durations.map((time) => {
+  const mins = +time.split(':')[0];
+  const secs = +time.split(':')[1];
+  return mins * 60 + secs;
+});
+
+const totalSeconds = timeSeconds.reduce((totalTime, nextValue) => totalTime + nextValue);
+const remainingSecs = totalSeconds % 60;
+let actualMinutes = Math.floor(totalSeconds / 60);
+const actualHours = Math.floor(actualMinutes / 60);
+actualMinutes %= actualHours * 60;
+
+console.log(`${actualHours}:${actualMinutes}:${remainingSecs}`);
